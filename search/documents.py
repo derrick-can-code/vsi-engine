44a326a274fbd8057c574eb51eb3772228bbd828
derrick-can-code <ermderrick18@gmail.com>
2026-07-15 02:43:42 +0000

    add search index builder and CLI search loop

---
+++ search/documents.py
@@ -0,0 +1,9 @@
+DOCUMENTS = {
+  0: "At Scale You Will Hit Every Performance Issue I used to think I knew...",
+  1: "Richard Stallman to visit Australia Im not usually one to promote...",
+  2: "MySQL Backups Done Easily One thing that comes up a lot on sites...",
+  3: "Why You Shouldnt roll your own CAPTCHA At a TechEd I attended...",
+  4: "The Great Benefit of Test Driven Development Nobody Talks About The...",
+  5: "Setting up GIT to use a Subversion SVN style workflow Moving from...",
+  6: "Why CAPTCHA Never Use Numbers 0 1 5 7 Interestingly this sort of...",
+}
